a3d6b37fd4ef0c026f76bb54a333454f83bd34be
sebastianjegorow <sebastian.jegorow@gruppe.ai>
2025-10-14 15:50:31 +0200

    wip.sh

--- wip.sh
+++ wip.sh
@@ -6,10 +6,15 @@ echo "STEMgraph repolist saved as ./repolist.txt"
 
 # loop through list and get each README.md
 while read -r p; do
-        if gh api /repos/STEMgraph/$p/contents/README.md --jq '.content' 2>/dev/null | base64 -d > readme.txt; then
-                echo "$p: "
-                grep depends_on readme.txt
-        else
-                echo "$p" >> no_readme.txt
-        fi
+    if gh api /repos/STEMgraph/$p/contents/README.md --jq '.content' 2>/dev/null | base64 -d > README.md; then
+
+# extract the meta-data for each result and dump it in a json file.
+	    
+    head -n 20 README.md | grep -Pzom1 '\{(?:[^{}"'\''\\]|\\.|"[^"\\]*(?:\\.[^"\\]*)*"|'\''[^'\''\\]*(?:\\.[^'\''\\]*)*'\''|(?0))*\}' | tr -d '\0' >> metadata_dump.json.tmp
+    
+
+	
+    else
+        echo "$p" >> no_readme.txt
+    fi
 done < repolist.txt
