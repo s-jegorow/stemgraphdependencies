d528ad3691c8d064bf6beed00dc6edeb847d6ba2
sebastianjegorow <sebastian.jegorow@gruppe.ai>
2025-10-15 09:10:26 +0200

    wip.sh updated (dependency parsing)

--- wip.sh
+++ wip.sh
@@ -6,15 +6,31 @@ echo "STEMgraph repolist saved as ./repolist.txt"
 
 # loop through list and get each README.md
 while read -r p; do
-    if gh api /repos/STEMgraph/$p/contents/README.md --jq '.content' 2>/dev/null | base64 -d > README.md; then
+  if gh api /repos/STEMgraph/"$p"/contents/README.md --jq '.content' 2>/dev/null | base64 -d > README.md; then
 
-# extract the meta-data for each result and dump it in a json file.
-	    
-    head -n 20 README.md | grep -Pzom1 '\{(?:[^{}"'\''\\]|\\.|"[^"\\]*(?:\\.[^"\\]*)*"|'\''[^'\''\\]*(?:\\.[^'\''\\]*)*'\''|(?0))*\}' | tr -d '\0' >> metadata_dump.json.tmp
-    
 
-	
-    else
-        echo "$p" >> no_readme.txt
+# Meta aus README ziehen
+    meta=$(head -n 20 README.md | grep -Pzom1 '\{(?:[^{}"'\''\\]|\\.|"[^"\\]*(?:\\.[^"\\]*)*"|'\''[^'\''\\]*(?:\\.[^'\''\\]*)*'\''|(?0))*\}' | tr -d '\0')
+
+
+# meta mit jq verarbeiten    
+    if [ -n "$meta" ]; then
+      printf '%s\n' "$meta" >> metadata_dump.json.tmp
+
+      {
+          printf "repo %s depends on:\n" "$p"
+	  printf '%s\n' "$meta" | jq -r '
+          .depends_on // [] |
+          (if type=="array" then .[] else . end) |
+          tostring |
+          sub(".*/"; "") |
+          select(. != "AND" and . != "")
+        '
+        echo
+      } >> deps.txt
     fi
+
+else
+    echo "$p" >> no_readme.txt
+  fi
 done < repolist.txt
